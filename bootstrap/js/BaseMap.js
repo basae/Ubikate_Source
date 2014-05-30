e134a7939d889eff13cedffb366bebd89599c779
Edrei Javier Bastar Sarao <basae_01@hotmail.com>
2014-05-30 16:11:54 -0500

    crate function init map and search places

---
+++ bootstrap/js/BaseMap.js
@@ -0,0 +1,126 @@
+var Control;
+var infowindow = new google.maps.InfoWindow();
+var mapControls=function (){
+
+	var MapRef;
+	var initialize=function(posx,posy,zoomp)
+		{
+			var mapOptions=
+				{
+					zoom:zoomp,
+					center:new google.maps.LatLng(posx,posy)
+				};
+
+			MapRef=new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+
+		};
+
+	var addMarker=function(posx,posy,style)
+	{
+		var optionMarker;
+			switch(style){
+
+				case "currentLocation":
+					optionMarker={
+						position:new google.maps.LatLng(posx,posy),
+						map:MapRef,
+						title:"Mi Localización",
+						icon:{
+      						path: google.maps.SymbolPath.CIRCLE,
+      						scale: 4,
+      						strokeColor:"#1223ff"
+   							},
+   						animation: google.maps.Animation.DROP
+					}
+				break;
+
+
+				default:
+				optionMarker={position:new google.maps.LatLng(posx,posy),map:MapRef}
+				break;
+
+			}
+
+
+		var marker=new google.maps.Marker(optionMarker);
+	};
+
+	function addPlaces(place)
+	{
+		 	var marker=new google.maps.Marker(
+			{
+						position:place.geometry.location,
+						map:MapRef,
+   						animation: google.maps.Animation.DROP
+
+					}
+			);
+
+		google.maps.event.addListener(marker, 'click', function() {
+		    infowindow.setContent(place.name+"<br/>Ubicación:"+place.vicinity);
+		    infowindow.open(MapRef, this);
+	  	});
+	}
+
+	var getPlaces=function(searchType){
+		var request = {
+    		location: MapRef.getCenter(),
+    		radius: 5000,
+    		types: [searchType]
+  			};
+
+  
+  		var service = new google.maps.places.PlacesService(MapRef);
+  		service.nearbySearch(request, callbackPlaces);
+	}
+
+	function callbackPlaces(results, status){
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+		    for (var i = 0; i < results.length; i++) {
+		      addPlaces(results[i]);
+		    }
+	 	}
+	}
+
+	return {
+		init:initialize,
+		addMarker:addMarker,
+		searchPlaces:getPlaces
+	};
+};
+
+$(function(){
+
+	if (navigator.geolocation) { /* Si el navegador tiene geolocalizacion */
+		Control=mapControls();
+                navigator.geolocation.getCurrentPosition(getPosition, errores);
+            }else{
+                alert('Oops! Tu navegador no soporta geolocalización. Bájate Chrome, que es gratis!');
+            }	
+});
+
+
+function getPosition(position)
+{
+	var lat=position.coords.latitude;
+	var lng=position.coords.longitude;
+	Control.init(lat,lng,14);
+	Control.addMarker(lat,lng,"currentLocation");
+}
+
+
+function errores(err) {
+    /*Controlamos los posibles errores */
+    if (err.code == 0) {
+      alert("Oops! Algo ha salido mal");
+    }
+    if (err.code == 1) {
+      alert("Oops! No has aceptado compartir tu posición");
+    }
+    if (err.code == 2) {
+      alert("Oops! No se puede obtener la posición actual");
+    }
+    if (err.code == 3) {
+      alert("Oops! Hemos superado el tiempo de espera");
+    }
+}
